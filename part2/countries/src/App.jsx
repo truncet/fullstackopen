b2a823ebc97072f4d67d8245371ff94741be0a2e
Ranjit Nepal <theonlyeminem@gmail.com>
2023-09-19 11:47:22 +0300

    Exercise 2.18 done

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,34 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import countriesService from './services/countries'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Countries = ({ allCountries }) => {
+  if (allCountries.length > 10) {
+    return (
+      <p>Too many matches, specify another filter</p>
+    );
+  } else if (allCountries.length === 1) {
+    console.log(allCountries[0])
+    let languages = Object.values(allCountries[0].languages);
+    return allCountries.map((country) => (
+      <div key={country.name.official}>
+        <h2>{country.name.common}</h2>
+        <p>capital {country.capital}</p>
+        <p>area {country.area}</p>
+
+        <h3>languages:</h3>
+        <ul>
+        {
+            languages.map((language) =>
+              <li key={language}>{language}</li>
+          )
+        }
+        </ul>
+        <img src={country.flags.png} alt={`Flag of ${country.flags.alt}`} />
+      </div>
+    ));
+
+  } else {
+    return allCountries.map((country) => (
+      <p key={country.name.official}> {country.name.common}</p>
+    ));
+  }
+};
+
+
+const App = () => {
+  const [countries, setCountries] = useState([]) 
+  const [searchValue, setSearchValue] = useState('')
+  const [showAll, setShowVal] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('')
+
+
+useEffect(() => {
+  countriesService.getAll().then (response => setCountries(response))
+}, [])
+
+  const countriesToShow = showAll
+    ? []
+    : countries.filter((country) => country.name.common.toLowerCase().includes(searchValue.toLowerCase()));
+
+  const handleSearchValue = (event) => {
+    let value = event.target.value;
+    setSearchValue(value);
+    setShowVal(value === "")
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1>Countries</h1>
+      <p>find countries <input value={searchValue} onChange={handleSearchValue}></input></p>
+
+      <Countries allCountries={countriesToShow}/>
+
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
